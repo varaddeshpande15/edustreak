df7a9027b9f52d02a41883fc2aab965d8fd898b4
Varad Deshpande <varaddeshpande81@gmail.com>
2025-07-26 22:25:12 +0530

    First Commit to Edustreak Agentic Version

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-const nextConfig = {
-    env: {
-      GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    },
-  };
-  
-  export default nextConfig;
-  
--- src/components/Spinner.js
+++
@@ -1,6 +0,0 @@
-const Spinner = () => (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-black"></div>
-    </div>
-  );
-export default Spinner;  
